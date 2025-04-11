3117d62b0b3c6dedc013183a8452e7808ae201f7
Ronald Martins <ronald.martins@gematik.de>
2025-04-11 15:04:24 +0000

    Added the Build

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 # Error handling: The script will terminate on error.
 set -e
-
+pip install build
 pip install shiv
+
+python -m build
 
 shiv -c epatools -o dist/epatools.pyz -e epatools.main:main .
